2033ab75b291d4d536c749875995a8d10679967a
VirtualBlakeWillis <bewillis101@gmail.com>
2022-08-16 11:15:42 -0700

    betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,11 @@
 #include "sort.h"
 
 /**
+ * quick_sort - quick sort algo
  *
+ * @array: int array to sort
+ * @size: size of array
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
@@ -12,17 +16,25 @@ void quick_sort(int *array, size_t size)
 
 	quick_sort_recursive(array, low, high, size);
 }
-
+/**
+ * quick_sort_recursive - recursive func for quick sort
+ *
+ * @array: array to sort
+ * @low: lowest index to sort
+ * @high: highest index to sort
+ * @size: size of array
+ * Return: array
+ */
 int *quick_sort_recursive(int *array, int low, int high, int size)
 {
 	int j, i, pivot, temp;
 
 	if (low >= high)
-		return array;
+		return (array);
 
 	pivot = array[high];
 	j = low;
-	for (i = low; i <+ high; i++)
+	for (i = low; i < high; i++)
 	{
 		if (array[i] <= pivot)
 		{
@@ -31,7 +43,7 @@ int *quick_sort_recursive(int *array, int low, int high, int size)
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
-				print_array(array,size);
+				print_array(array, size);
 			}
 			j += 1;
 		}
