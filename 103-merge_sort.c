97b2b40dea654ec5e99cccc02e643baeb78c658a
wilstermanz <wilstermanz@gmail.com>
2023-01-17 19:57:46 -0600

    Solution for task 0

---
+++ 103-merge_sort.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+#include <stdio.h>
+
+void merge(int *leftArray, size_t leftSize, int *rightArray, size_t rightSize);
+
+/**
+ * merge_sort - merge sort recursively
+ *
+ * @array: int array to sort
+ * @size: size of array
+ * Return: void
+ */
+
+void merge_sort(int *array, size_t size)
+{
+	int *leftArray = &array[0];
+	int *rightArray = &array[size / 2];
+	int leftSize = size / 2;
+	int rightSize = size - leftSize;
+
+	if (!array || size <= 1)
+		return;
+
+	merge_sort(leftArray, leftSize);
+	merge_sort(rightArray, rightSize);
+
+	merge(leftArray, leftSize, rightArray, rightSize);
+}
+
+/**
+ * merge - merges arrays
+ *
+ * @leftArray: left array
+ * @leftSize: size of left array
+ * @rightArray: right array
+ * @rightSize: size of right array
+ * Return: void
+ */
+
+void merge(int *leftArray, size_t leftSize, int *rightArray, size_t rightSize)
+{
+	int *tempArray = malloc(sizeof(leftArray[0]) * (leftSize + rightSize));
+	int tempSize = leftSize + rightSize;
+	int i = 0, j = 0, k = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(leftArray, leftSize);
+	printf("[right]: ");
+	print_array(rightArray, rightSize);
+	while (leftSize && rightSize)
+	{
+		if (leftArray[i] < rightArray[j])
+		{
+			tempArray[k] = leftArray[i];
+			i++, k++, leftSize--;
+		}
+		else
+		{
+			tempArray[k] = rightArray[j];
+			j++, k++, rightSize--;
+		}
+	}
+	while (leftSize)
+	{
+		tempArray[k] = leftArray[i];
+		i++, k++, leftSize--;
+	}
+	while (rightSize)
+	{
+		tempArray[k] = rightArray[j];
+		j++, k++, rightSize--;
+	}
+	for (i = 0; i < tempSize; i++)
+		leftArray[i] = tempArray[i];
+	free(tempArray);
+	printf("[Done]:");
+	print_array(leftArray, tempSize);
+}
--- sort.h
+++ sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -25,7 +25,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 int *quick_sort_recursive(int *array, int low, int high, int size);
 void quick_sort(int *array, size_t size);
-
 void swap(listint_t **head, listint_t **node1, listint_t **node2);
+
+void merge_sort(int *array, size_t size);
 
 #endif
