42839f0f161117458cc8fd22c9de6ec09a98fe0e
wilstermanz <wilstermanz@gmail.com>
2023-01-18 19:03:20 -0600

    prints swaps correctly

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 void build_max_heap(int *array, size_t heapSize);
-void heapify(int *array, size_t heapSize, size_t parentIndex);
-void swap_nodes(int *array, size_t indexA, size_t indexB);
+void heapify(int *array, size_t size, size_t heapSize, size_t parentIndex);
+void swap_nodes(int *array, size_t size, size_t indexA, size_t indexB);
 
 /**
  * heap_sort - Sorts an array of numbers using the heap method
@@ -22,8 +22,8 @@ void heap_sort(int *array, size_t size)
 	build_max_heap(array, size);
 	for (heapSize = size; heapSize > 0; heapSize--)
 	{
-		swap_nodes(array, 0, heapSize - 1);
-		heapify(array, heapSize, 0);
+		swap_nodes(array, size, 0, heapSize - 1);
+		heapify(array, size, heapSize, 0);
 	}
 }
 
@@ -39,7 +39,7 @@ void build_max_heap(int *array, size_t size)
 	int nodeIndex;
 
 	for (nodeIndex = size / 2 - 1; nodeIndex >= 0; nodeIndex--)
-		heapify(array, size, nodeIndex);
+		heapify(array, size, size, nodeIndex);
 }
 
 /**
@@ -47,11 +47,12 @@ void build_max_heap(int *array, size_t size)
  * and recursively sifts smaller values down
  *
  * @array: entire array
+ * @size: array size
  * @heapSize: size of heap within array
  * @parentIndex: index of parent node
  *
  */
-void heapify(int *array, size_t heapSize, size_t parentIndex)
+void heapify(int *array, size_t size, size_t heapSize, size_t parentIndex)
 {
 	size_t leftNodeIndex = parentIndex * 2 + 1;
 	size_t rightNodeIndex = leftNodeIndex + 1;
@@ -67,8 +68,8 @@ void heapify(int *array, size_t heapSize, size_t parentIndex)
 
 	if (largestIndex != parentIndex)
 	{
-		swap_nodes(array, largestIndex, parentIndex);
-		heapify(array, heapSize, largestIndex);
+		swap_nodes(array, size, largestIndex, parentIndex);
+		heapify(array, size, heapSize, largestIndex);
 	}
 }
 
@@ -76,15 +77,16 @@ void heapify(int *array, size_t heapSize, size_t parentIndex)
  * swap_nodes - Swaps two values in an int array
  *
  * @array: array of ints
+ * @size: array size
  * @indexA: first value to swap
  * @indexB: second value to swap
  *
  */
-void swap_nodes(int *array, size_t indexA, size_t indexB)
+void swap_nodes(int *array, size_t size, size_t indexA, size_t indexB)
 {
 	int temp = array[indexA];
 
 	array[indexA] = array[indexB];
 	array[indexB] = temp;
-	print_array(array, 10);
+	print_array(array, size);
 }
